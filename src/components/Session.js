863ef58768451103492a02b5fbff3606bfa02b6f
Luis Rosario <luisrosariomix@gmail.com>
2019-08-09 22:06:48 -0700

    Added React Boostrap Library

--- src/components/Session.js
+++ src/components/Session.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react'
+import { Carousel, Item, Caption } from 'react-bootstrap'
 
 class Session extends React.Component {
   render() {
     return (
         <>
-      <h1>{this.props.theSession.name}</h1>
+          <Carousel.Item>
+            <img
+            className="d-block w-100"
+            src={this.props.theSession.img_url}
+            alt="First slide"
+            />
+          <Carousel.Caption>
+            <h3>{this.props.theSession.name}</h3>
+            <h4>{this.props.theSession.modality}</h4>
+            <p>{this.props.theSession.description}</p>
+          </Carousel.Caption>
+          </Carousel.Item>
         </>
     )
   }
--- src/components/Sessions.js
+++ src/components/Sessions.js
@@ -1,21 +1,32 @@
 import React, { Component } from 'react'
 import Session from './Session.js'
+import Carousel from 'react-bootstrap/Carousel'
 
 class Sessions extends React.Component {
   render() {
     return (
-      <div>
-        {
-          this.props.sessions.map((theSession) => {
-            return(
-              <Session
-                key={ theSession.id }
-                theSession={ theSession }
-              />
-            )
-          })
-        }
-      </div>
+      <>
+        <Carousel>
+          {
+            this.props.sessions.map((theSession) => {
+              return(
+                <Carousel.Item>
+                  <img
+                  className="d-block w-100"
+                  src={theSession.img_url}
+                  alt="First slide"
+                  />
+                <Carousel.Caption>
+                  <h3>{theSession.name}</h3>
+                  <h4>{theSession.modality}</h4>
+                  <p>{theSession.description}</p>
+                </Carousel.Caption>
+                </Carousel.Item>
+                )
+              })
+            }
+        </Carousel>
+      </>
     )
   }
 }
