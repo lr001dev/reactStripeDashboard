7834f4a646a8bc342199874abdebff52e2c2f23e
Luis Rosario <luisrosariomix@gmail.com>
2019-08-14 21:09:31 -0700

    Added Logout Redirect

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ class App extends React.Component {
       const newState = {
         sessions: this.state.sessions
       }
-      this.setState({[...newState] })
+      this.setState({ })
     }).catch(err=> console.log(err))
   }
   getSessions() {
